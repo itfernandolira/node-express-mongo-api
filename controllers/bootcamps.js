ad774ad08fd6d0be2fc0fb4869c10ac390d3b885
Fernando Lira <it.fernandolira@gmail.com>
2021-10-09 18:47:43 +0100

    Error Handler Middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -31,7 +31,8 @@ exports.getBootcamps = async (req, res, next) => {
   
       res.status(200).json({ success: true, data: bootcamp });
     } catch (err) {
-      res.status(400).json({ success: false });
+      //res.status(400).json({ success: false });
+      next(err);
     }
 
   };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 const colors = require('colors');
 
@@ -25,6 +26,8 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
