cd787715ed9d285c6e3405d5e5dd2033d24f5901
Fernando Lira <it.fernandolira@gmail.com>
2021-10-09 14:54:26 +0100

    Bootcamp List All and List One

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,18 +3,37 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
 // @access    Public
-exports.getBootcamps = (req, res, next) => {  
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => { 
+
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res
+      .status(200)
+      .json({ success: true, count: bootcamps.length, data: bootcamps });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 
   };
   
   // @desc      Get single bootcamp
   // @route     GET /api/v1/bootcamps/:id
   // @access    Public
-  exports.getBootcamp = (req, res, next) => {
-    res
-      .status(200)
-      .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+  exports.getBootcamp = async (req, res, next) => {
+    
+    try {
+      const bootcamp = await Bootcamp.findById(req.params.id);
+  
+      if (!bootcamp) {
+        return res.status(400).json({ success: false });
+      }
+  
+      res.status(200).json({ success: true, data: bootcamp });
+    } catch (err) {
+      res.status(400).json({ success: false });
+    }
+
   };
   
   // @desc      Create new bootcamp
