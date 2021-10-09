478e69c8a226570e857c0cd042d59e750df1001d
Fernando Lira <it.fernandolira@gmail.com>
2021-10-09 15:27:38 +0100

    Update and Delete Bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -59,17 +59,38 @@ exports.getBootcamps = async (req, res, next) => {
   // @desc      Update bootcamp
   // @route     PUT /api/v1/bootcamps/:id
   // @access    Private
-  exports.updateBootcamp = (req, res, next) => {
-    res
-      .status(200)
-      .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+  exports.updateBootcamp = async (req, res, next) => {
+    
+    try {
+      const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+      });
+  
+      if (!bootcamp) {
+        return res.status(400).json({ success: false });
+      }
+  
+      res.status(200).json({ success: true, data: bootcamp });
+    } catch (err) {
+      res.status(400).json({ success: false });
+    }
+
   };
   
   // @desc      Delete bootcamp
   // @route     DELETE /api/v1/bootcamps/:id
   // @access    Private
-  exports.deleteBootcamp = (req, res, next) => {
-    res
-      .status(200)
-      .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+  exports.deleteBootcamp = async (req, res, next) => {
+    try {
+      const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  
+      if (!bootcamp) {
+        return res.status(400).json({ success: false });
+      }
+  
+      res.status(200).json({ success: true, data: {} });
+    } catch (err) {
+      res.status(400).json({ success: false });
+    }
   };
