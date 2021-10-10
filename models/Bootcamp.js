fa058afa228639368f660e4aebf0a4b309b9379b
Fernando Lira <it.fernandolira@gmail.com>
2021-10-10 11:31:50 +0100

    Mongoose Midlleware - Slugify

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -97,5 +98,12 @@ const BootcampSchema = new mongoose.Schema({
     default: Date.now
   }
 });
+
+// Create bootcamp slug from the name
+BootcampSchema.pre('save', function(next) {
+    console.log('slugify',this.name);
+    this.slug = slugify(this.name, { lower: true });
+    next();
+  });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
