fba36ed560539feb892b6547187294b360e97886
Fernando Lira <it.fernandolira@gmail.com>
2021-10-10 15:04:57 +0100

    Node-geocoder and MapQuest

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -103,6 +104,26 @@ const BootcampSchema = new mongoose.Schema({
 BootcampSchema.pre('save', function(next) {
     console.log('slugify',this.name);
     this.slug = slugify(this.name, { lower: true });
+    next();
+  });
+
+  // Geocode & create location field
+BootcampSchema.pre('save', async function(next) {
+    console.log('GEOCODE:',process.env.GEOCODER_PROVIDER);
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+      type: 'Point',
+      coordinates: [loc[0].longitude, loc[0].latitude],
+      formattedAddress: loc[0].formattedAddress,
+      street: loc[0].streetName,
+      city: loc[0].city,
+      state: loc[0].stateCode,
+      zipcode: loc[0].zipcode,
+      country: loc[0].countryCode
+    };
+  
+    // Do not save address in DB
+    this.address = undefined;
     next();
   });
 
---
+++ utils/geocoder.js
@@ -0,0 +1,16 @@
+const NodeGeocoder = require('node-geocoder');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+const options = {
+  provider: process.env.GEOCODER_PROVIDER,
+  httpAdapter: 'https',
+  apiKey: process.env.GEOCODER_API_KEY,
+  formatter: null
+};
+
+const geocoder = NodeGeocoder(options);
+
+module.exports = geocoder;
