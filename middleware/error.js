d044fbef676dec6dcff1bf98ef0a725e3449ac63
Fernando Lira <it.fernandolira@gmail.com>
2021-10-10 09:47:46 +0100

    Mongoose Error Handling - Part 2

--- middleware/error.js
+++ middleware/error.js
@@ -9,11 +9,24 @@ const errorHandler = (err, req, res, next) => {
     //ver o nome do erro
     console.log(err.name);
     console.log(error.name);
+    console.log(err);
 
     // Mongoose bad ObjectId - erro CastError
     if (err.name === 'CastError') {
         const message = `Bootcamp not found withh id of ${err.value}`;
         error = new ErrorResponse(message, 404);
+    }
+
+    // Mongoose duplicate key
+    if (err.code === 11000) {
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400);
+    }
+
+      // Mongoose validation error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400);
     }
 
     res.status(error.statusCode || 500).json({
