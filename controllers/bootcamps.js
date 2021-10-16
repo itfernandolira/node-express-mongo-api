b23b7b8f9afc37773a678c9393257738c31169f5
Fernando Lira <it.fernandolira@gmail.com>
2021-10-16 09:39:05 +0100

    Adding pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -18,7 +18,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query };
 
   // Fields to exclude
-  const removeFields = ['select', 'sort'];
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
   // Loop over removeFields and delete them from reqQuery
   removeFields.forEach(param => delete reqQuery[param]);
@@ -45,13 +45,46 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
       query = query.sort('-createdAt');
     }
 
+    // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
   const bootcamps = await query;
   
   //const bootcamps = await Bootcamp.find();
 
-    res
+    /* res
       .status(200)
-      .json({ success: true, count: bootcamps.length, data: bootcamps });
+      .json({ success: true, count: bootcamps.length, data: bootcamps }); */
+
+      // Pagination result
+    const pagination = {};
+
+    if (endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit
+      };
+    }
+
+    if (startIndex > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit
+      };
+    }
+
+    res.status(200).json({
+      success: true,
+      count: bootcamps.length,
+      pagination,
+      data: bootcamps
+    });
 
   });
   
