8b64ea4e9d9e867b6f6b79ee5fbd09b0c047b4db
Fernando Lira <it.fernandolira@gmail.com>
2021-10-16 12:58:51 +0100

    Course routes and getCourses

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const colors = require('colors');
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -26,6 +27,7 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 
 app.use(errorHandler);
 
