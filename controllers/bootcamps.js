ac00649f3d163e463a5a851ed10ddb312dfff258
Fernando Lira <it.fernandolira@gmail.com>
2021-10-16 19:57:08 +0100

    Cascade delete

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -139,11 +139,14 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   // @access    Private
   exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 
-      const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+      //const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+      const bootcamp = await Bootcamp.findById(req.params.id);
   
       if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
       }
+
+      bootcamp.remove();
   
       res.status(200).json({ success: true, data: {} });
 
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -132,6 +132,13 @@ BootcampSchema.pre('save', async function(next) {
     next();
   });
 
+  // Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function(next) {
+  console.log(`Courses being removed from bootcamp ${this._id}`);
+  await this.model('Course').deleteMany({ bootcamp: this._id });
+  next();
+});
+
   // Reverse populate with virtuals
 BootcampSchema.virtual('courses', {
   ref: 'Course',
