4d7f45b79c4aa4791b855944885fb2222a187bff
Fernando Lira <it.fernandolira@gmail.com>
2021-10-19 17:31:22 +0100

    Photo Upload

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const errorHandler = require('./middleware/error');
+const fileupload = require('express-fileupload');
 const connectDB = require('./config/db');
 const colors = require('colors');
 
@@ -24,6 +26,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// File uploading
+app.use(fileupload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
