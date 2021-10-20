d25d3131fffbbca97b4bfeb256c45288a01376cc
Fernando Lira <it.fernandolira@gmail.com>
2021-10-20 12:33:50 +0100

    Advanced Results Middleware

--- controllers/courses.js
+++ controllers/courses.js
@@ -8,27 +8,22 @@ const Bootcamp = require('../models/Bootcamp');
 // @route     GET /api/v1/bootcamps/:bootcampId/courses
 // @access    Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  let query;
+  
 
   if (req.params.bootcampId) {
-    query = Course.find({ bootcamp: req.params.bootcampId });
-  } else {
-    //query = Course.find();
-    //Todos os dados do bootcamp
-    //query = Course.find().populate('bootcamp');
-    query = Course.find().populate({
-      path: 'bootcamp',
-      select: 'name description'
+
+    const courses = await Course.find({ bootcamp: req.params.bootcampId });
+
+    return res.status(200).json({
+      success: true,
+      count: courses.length,
+      data: courses
     });
+    
+  } else {
+    res.status(200).json(res.advancedResults);
   }
 
-  const courses = await query;
-
-  res.status(200).json({
-    success: true,
-    count: courses.length,
-    data: courses
-  });
 });
 
 // @desc      Get single course
