cbd6138519cbed5d61e7092707b51444eada8b4a
Fernando Lira <it.fernandolira@gmail.com>
2021-10-24 21:47:55 +0100

    API Security

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const errorHandler = require('./middleware/error');
 const fileupload = require('express-fileupload');
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const colors = require('colors');
 
 // Route files
@@ -35,6 +41,29 @@ if (process.env.NODE_ENV === 'development') {
 
 // File uploading
 app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
