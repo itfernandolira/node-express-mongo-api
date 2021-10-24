54653850590d15013d7600e036f714214f0b26ef
Fernando Lira <it.fernandolira@gmail.com>
2021-10-24 21:43:10 +0100

    Logout to clear cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -94,3 +94,18 @@ exports.getMe = asyncHandler(async (req, res, next) => {
     data: user
   });
 });
+
+// @desc      Log user out / clear cookie
+// @route     GET /api/v1/auth/logout
+// @access    Private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, getMe } = require('../controllers/auth');
+const { register, login, logout,getMe } = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ const { protect } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.get('/logout', protect, logout);
 
 module.exports = router;
